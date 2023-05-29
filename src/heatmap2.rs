ca71458f3b42f9ea7c9fff1f62fd3e4703ad3d1c
Luiz Maurício <2003lmvp@gmail.com>
2023-05-29 13:06:22 -0300

    Heatmap according to Odorico mostly done

--- src/heatmap2.rs
+++ src/heatmap2.rs
@@ -1,36 +1,47 @@
 /*
- Heatmap data structure as suggested by my advisor
+ Heatmap var structure as suggested by my advisor
  */
 
 #[derive(Debug)]
 struct Heatmap {
-    data: Vec<String>,          // Vector that represents the variables acessed by a requisition
+    var: Vec<String>,               // Vector that represents the variables acessed by a requisition
+    timeblock_number: usize,
     timeblock_size: usize,        // block of reqs that make a single timespan, number of reqs inside such block
-    frequency: Vec<Vec<u64>>    // Matrix responsible for storing the frequencies of data in a timeblock
+    frequency: Vec<Vec<u64>>    // Matrix responsible for storing the frequencies of var in a timeblock
 }
 
 impl Heatmap {
     fn new(timeblock_size: usize) -> Heatmap {
         Heatmap {
-            data: Vec::new(),
+            var: Vec::new(),
+            timeblock_number: 0,
             timeblock_size,
             frequency: Vec::new()
         }
     }
 
-    fn get_data(&self) -> &Vec<String> {
-        &self.data
+    fn get_var(&self) -> &Vec<String> {
+        &self.var
     }
 
-    fn get_mut_data(&mut self) -> &mut Vec<String> {
-        &mut self.data
+    fn get_mut_var(&mut self) -> &mut Vec<String> {
+        &mut self.var
     }
 
-    fn add_data(&mut self, data: String) -> () {
-        let mut_data = self.get_mut_data();
-        mut_data.push(data);
+    fn equalize_lines(&mut self) -> () {
+        for i in 0..self.var.len() {
+            while self.frequency[i].len() < self.timeblock_number {
+                self.frequency[i].push(0)
+            }
+        }
+    }
+
+    fn add_var(&mut self, var: String) -> () {
+        let mut_var = self.get_mut_var();
+        mut_var.push(var);
         let temp_vec:Vec<u64> = Vec::new();
-        self.frequency.push(temp_vec)
+        self.frequency.push(temp_vec);
+        self.equalize_lines()
     }
 
     fn get_timeblock_size(&self) -> &usize {
@@ -44,11 +55,24 @@ impl Heatmap {
     fn get_frequency_at(&self, i:usize, j:usize) -> &u64 {
         &self.frequency[i][j]
     }
+
+    fn add_data(&mut self, i:usize, j:usize, req_number: usize) -> () {
+        if req_number%self.timeblock_size == 0 {
+            self.timeblock_number += 1;
+        }
+        self.equalize_lines();
+        self.frequency[i][j] += 1
+    }
 }
 
 fn main() {
     let mut heatmap = Heatmap::new(4);
-    heatmap.add_data("data".to_string());
-    heatmap.add_data("data2".to_string());
+    heatmap.add_var("var".to_string());
+    heatmap.add_var("var2".to_string());
+    heatmap.add_data(1, 0, 0);
+    heatmap.add_data(1, 0, 1);
+    heatmap.add_data(1, 0, 2);
+    heatmap.add_data(1, 0, 3);
+    heatmap.add_data(1, 1, 4);
     println!("{:?}", heatmap)
 }

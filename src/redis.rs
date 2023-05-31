d40bccbd7bf233779f2f416c0991ba11dd3fdd96
Luiz Maurício <2003lmvp@gmail.com>
2023-05-31 15:05:10 -0300

    starting redis specific implementation

---
+++ src/redis.rs
@@ -0,0 +1,8 @@
+mod heatmap2;
+mod parser;
+
+impl Redis for ImplementationSpecificOperations {
+    fn parse_into(&self, filepath: &str) -> () {
+        let mut fila: VecDeque<Vec<String>> = VecDeque::new();
+    }
+}

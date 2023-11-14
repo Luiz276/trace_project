a3699123e2aa661737b29a1eb66c9901115cf93c
Luiz Maurício <2003lmvp@gmail.com>
2023-11-14 15:56:25 -0300

    change log and implementation_specific... name

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ mod generator;
 use std::collections::VecDeque;
 use heatmap::heatmap2::Heatmap;
 use crate::heatmap::parser;
-use crate::generator::redis::implementationSpecificOperations;
+use crate::generator::redis::implementation_specific_operations;
 
-static FILEPATH: &str = "./redis_get_set.log";
+static FILEPATH: &str = "./redis_test.log";
 
 #[allow(dead_code)]
 fn print_fila_commands(fila: &mut VecDeque<Vec<String>>) -> () {
@@ -44,17 +44,10 @@ fn main() {
     }
 
     let mut lista_heatmaps: Vec<Heatmap> = Vec::new();
-    lista_heatmaps = implementationSpecificOperations::create_heatmaps(lista_heatmaps, fila_linhas_log.clone());
+    lista_heatmaps = implementation_specific_operations::create_heatmaps(lista_heatmaps, fila_linhas_log.clone());
 
     print_fila_commands(&mut fila_linhas_log);
     print_fila_tempos(&mut fila_tempos);
-    //reqs::send_command(fila);
-    // let heatmap = Heatmap::new("GET".to_string(), 4);
-    // print_heatmap(&heatmap);
-    // println!("{:?}", heatmap);
+    
     println!("program OK")
-    
-    //let heatmap_list = parser::
-    
-    //parser::parse("redis_get_set.log")
 }

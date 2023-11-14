1a660ae8dae3cf2889c2be62a0d91cb39edea768
Luiz Maurício <2003lmvp@gmail.com>
2023-11-14 15:57:10 -0300

    change implementation_specific.. name and heatmap API

--- src/generator/redis.rs
+++ src/generator/redis.rs
@@ -1,7 +1,8 @@
 // mod heatmap2;
 // pub mod parser;
 
-pub mod implementationSpecificOperations {
+pub mod implementation_specific_operations {
+
     use std::collections::VecDeque;
 
     use crate::Heatmap;
@@ -15,9 +16,18 @@ pub mod implementationSpecificOperations {
     //     }
 
     // }
+    fn find_htmp(lista_heatmaps:&mut Vec<Heatmap>, command: String) -> Result<&mut Heatmap, &'static str> {
+        for htmp in lista_heatmaps {
+            if htmp.get_command() == &command {
+                return Ok(htmp)
+            }
+        }
+        return Err("No htmp in lista_htmp");
+    }
 
     pub fn create_heatmaps(mut lista_heatmaps: Vec<Heatmap>, mut fila_linhas_log: VecDeque<Vec<String>>) -> Vec<Heatmap> {
         let mut vec_commands: Vec<String> = Vec::new();
+        let i = 0;
 
         while !fila_linhas_log.is_empty() {
             let linha = fila_linhas_log.pop_front();
@@ -30,14 +40,14 @@ pub mod implementationSpecificOperations {
                         vec_commands.push(command.clone());
                         lista_heatmaps.push(Heatmap::new(command, 4))
                     }
-                    let mut target_htmp: &Heatmap;
-                    for htmp in &lista_heatmaps[..] {
-                        if htmp.get_command() == &temp_command {
-                            target_htmp = htmp;
-                            break;
-                        }
+                    let mut target_htmp;
+                    match find_htmp(&mut lista_heatmaps, temp_command) {
+                        Ok(htmp) => target_htmp = htmp,
+                        Err(error) => panic!("{:?}",error)
                     }
+                    // lista_heatmaps.into_iter().find(x.command == &temp_command);
                     /* TODO: Parse data into the previously created heatmaps */
+                    target_htmp.add_data(line[4].clone(), i)
                 },
                 None => println!("Empty line in log"),
             }
